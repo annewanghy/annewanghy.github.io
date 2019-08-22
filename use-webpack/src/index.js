19a982e649d5325dedb36c655038733aba4eaec0
AnneWang <annewang@theplant.jp>
2019-08-22 10:20:42 +0800

    Add output config

--- use-webpack/src/index.js
+++ use-webpack/src/index.js
@@ -1,9 +1,15 @@
 import _ from "lodash";
+import printMe from "./print.js";
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
 
   element.innerHTML = _.join(["hello", "webpack"], "");
+
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
   return element;
 }
