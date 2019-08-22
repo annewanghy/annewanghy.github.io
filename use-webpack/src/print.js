c413b4139d99ac3ac97e18221e9e535ffe8ff575
AnneWang <annewang@theplant.jp>
2019-08-22 10:37:09 +0800

    Add webpack-dev-serve to auto see changes

--- use-webpack/src/print.js
+++ use-webpack/src/print.js
@@ -1,4 +1,4 @@
 export default function printMe() {
   console.log("I get called from print.js");
-  console.log("aha you");
+  console.log("aha");
 }
--- use-webpack/webpack.config.js
+++ use-webpack/webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
     print: "./src/print.js"
   },
   devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
