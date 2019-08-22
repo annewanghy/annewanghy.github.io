ca9dc8abdb7642b25cf31569567790dff1253a83
AnneWang <annewang@theplant.jp>
2019-08-22 09:58:33 +0800

    Add css

--- use-webpack/src/index.js
+++ use-webpack/src/index.js
@@ -1,9 +1,12 @@
 import _ from "lodash";
+import "./style.css";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["hello", "webpack"], "");
+
+  element.classList.add("hello");
 
   return element;
 }
