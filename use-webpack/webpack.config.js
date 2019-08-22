9dad28d63b69f4b105364b9974ab0a0906a046f5
AnneWang <annewang@theplant.jp>
2019-08-22 10:31:17 +0800

    Development with source map

--- use-webpack/webpack.config.js
+++ use-webpack/webpack.config.js
@@ -3,10 +3,12 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     app: "./src/index.js",
     print: "./src/print.js"
   },
+  devtool: "inline-source-map",
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
