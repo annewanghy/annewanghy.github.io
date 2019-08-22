b58604f1327ac86ab250cedd88c5c2a8e5c516aa
AnneWang <annewang@theplant.jp>
2019-08-22 10:50:31 +0800

    Make element to be updated

--- use-webpack/src/index.js
+++ use-webpack/src/index.js
@@ -14,11 +14,14 @@ function component() {
   return element;
 }
 
-document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
 
 if (module.hot) {
   module.hot.accept("./print", function() {
     console.log("Accepting the updated printMe module!");
-    printMe();
+    document.body.removeChild(element);
+    element = component();
+    document.body.appendChild(element);
   });
 }
