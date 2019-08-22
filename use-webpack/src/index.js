5333092d87e2d1e08e894f03fb63adfdfbe7cdd1
AnneWang <annewang@theplant.jp>
2019-08-22 10:14:47 +0800

    Clean up

--- use-webpack/src/index.js
+++ use-webpack/src/index.js
@@ -1,21 +1,11 @@
 import _ from "lodash";
-import "./style.css";
-import MyImage from "./my-image.png";
-import data from "./data.json";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["hello", "webpack"], "");
 
-  element.classList.add("hello");
-
-  const myImage = new Image();
-  myImage.src = MyImage;
-  element.appendChild(myImage);
-
   return element;
 }
 
 document.body.appendChild(component());
-console.log(data);
--- use-webpack/webpack.config.js
+++ use-webpack/webpack.config.js
@@ -5,17 +5,5 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"]
-      }
-    ]
   }
 };
