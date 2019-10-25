232e14fdeab97f5d57237c43652417d641c78ff0
AnneWang <annewang@theplant.jp>
2019-10-25 17:09:17 +0800

    Add next js demo

---
+++ use-next/my-app/components/nav.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const Preload = ({}) => {
+  return (
+    <Head>
+      {/* <script
+        src="https://assets.demo.qorpersonalization.com/boost-6000.min.js"
+        async
+      /> */}
+      <script
+        src="https://assets.demo.qorpersonalization.com/boost-5000.min.js"
+        async
+      />
+    </Head>
+  );
+};
+
+export { Preload };
