0a92e3151c5446e040b9aa46eace59d7c5299b56
AnneWang <annewang@theplant.jp>
2019-10-25 18:07:27 +0800

    Use component instead of direct set script in Head to fixexecute twice

--- use-next/my-app/pages/index.js
+++ use-next/my-app/pages/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Head from "next/head";
 
+const Ext2 = () => <script src="/static/ext2.js" async></script>;
+
 const Home = () => (
   <div>
     <Head>
-      {/* 
-      Scripts with async in Head are executed twice 
-      https://github.com/zeit/next.js/issues/9070:
-      one workaround is to set async="" */}
-      <script src="https://annewanghy.github.io/inline-style.js" async="" />
+      {/* refer:https://github.com/zeit/next.js/issues/9070 */}
+      <script src="/static/ext.js"></script>
+      {/* Use component instead of direct set script in Head to fixexecute twice */}
+      <Ext2></Ext2>
     </Head>
   </div>
 );
---
+++ use-next/my-app/static/ext2.js
@@ -0,0 +1,1 @@
+alert("Hello from ext2.js")
