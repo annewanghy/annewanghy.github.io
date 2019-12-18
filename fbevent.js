bbb5b60d72a7de88fa9117476a8d3cfe84cbefee
AnneWang <annewang@theplant.jp>
2019-12-18 16:27:48 +0800

    Comment part of ma

--- fbevent.js
+++ fbevent.js
@@ -8515,11 +8515,9 @@ fbq.pendingConfigs = ["global_config"];
             });
 
           function ma() {
-            console.log(
-              "enter ma, not comment part of pushState, and replace state"
-            );
+            console.log("enter ma,", "k", k, "b", b);
             if (a.disablePushState === !0) return;
-            if (!k.pushState || !k.replaceState) return;
+            // if (!k.pushState || !k.replaceState) return;
             var b = v(function() {
               Q = L;
               L = i.href;
@@ -8533,8 +8531,7 @@ fbq.pendingConfigs = ["global_config"];
             console.log("comment u.call pageview");
             u(k, "pushState", b);
             u(k, "replaceState", b);
-            console.log("set popstate passive to true");
-            g.addEventListener("popstate", b, !1);
+            // g.addEventListener("popstate", b, !1);
           }
           G.listenOnce(function() {
             ma();
